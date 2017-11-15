7762a52522f367b7741b7e9314d5348831f9959c
DishaDungrani <33677537+DishaDungrani@users.noreply.github.com>
2017-11-14 21:25:31 -0800

    Update one_more_file.rb

--- one_more_file.rb
+++ one_more_file.rb
@@ -1,5 +1,32 @@
-class OneMoreFile
+require 'rubygems'
+require 'appium_lib'
 
-  puts ' I Commited my code to git and in the event the machine is lost or stolen I am safe'
+#
+#gem install selenium-webdriver
+#sudo gem install selenium-webdriver
+#gem update --system
+#sudo gem install rails
+
+#gem install appium_lib
+#Saraths-MBP:bin root# sudo gem install appium_lib -n /usr/local/bin/
+#
+#
+#
+
+
+class one_more_file
+
+  desired_caps = {
+      caps:  {
+          platformName:  'Android',
+          platformVersion: '5.1',
+          deviceName:    'Nexus_5_API_22_x86',
+          browserName:   'Browser',
+      }
+  }
+  @appium_driver = Appium::Driver.new(desired_caps)
+  @selenium_driver = @appium_driver.start_driver
+  @selenium_driver.get("http://www.google.com/")
+  @selenium_driver.get("http://www.google.com/")
+
 end
-
