e0e22cdf424f3f5cd357daa580b7b86ed0e064b8
DishaDungrani <disha7002@gmail.com>
2018-01-23 00:17:24 -0500

    added

--- sample_test.rb
+++ sample_test.rb
@@ -16,23 +16,29 @@ class SampleTest < Test::Unit::TestCase
     wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
     @element = wait.until { @driver.find_element(:id => "lst-ib") }
 #searchBox=@driver.find_element(:id,'st-ib')
-#input text
-    @element.send_keys('SearchText')
-    sleep 2
+#input text  sleep 5
+    @element.send_keys('apple')
+    sleep 5
     @driver.action.send_keys(:enter).perform
-
+    sleep 5
 
     @element = wait.until { @driver.find_element(:xpath => "//*[@id='rso']/div/div/div[1]/div/div/h3/a") }
+    sleep 5
+    if(@element.text.include?("Apple"))
+      puts "Page contains the text apple"
 
-    assert(@element.text.include?("angular"),"Page contains the text angular")
+    end
+    assert(@element.text.include?("test"),"Page contains the text apple")
+    sleep 5
     puts @element.text
 
 
     # make some changes in a code
     # test cases
-    # Code changes
-
+    # Code changes/Users/ddungrani-/RubymineProjects/firstRubyProject
     sleep 5
+
+    # Hello this line is for comment, iot shouldn't show up for output.
 
   end
 end
